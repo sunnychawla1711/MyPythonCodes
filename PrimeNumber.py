380fc63411e06949f411e0b435736829eee64369
Sunny Chawla <chawla.sunny1711@gmail.com>
2025-12-26 14:16:49 +0530

    Add files via upload

---
+++ PrimeNumber.py
@@ -0,0 +1,15 @@
+num = int(input("Enter a number: "))
+
+if num <= 1:
+    print(num, "is not a Prime number")
+else:
+    is_prime = True
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+
+    if is_prime:
+        print(num, "is a Prime number")
+    else:
+        print(num, "is not a Prime number")
